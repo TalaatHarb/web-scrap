4953d8e93ce0ff606fff93a3a4a3583c62e4bcac
mharb <eng.talat.harb@gmail.com>
2023-01-12 21:42:47 +0200

    added template java project

---
+++ src/main/java/net/talaatharb/webscrap/WebScrappingApplication.java
@@ -0,0 +1,10 @@
+package net.talaatharb.webscrap;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class WebScrappingApplication {
+	public static void main(String[] args) {
+        log.info("Application Started");
+    }
+}
---
+++ src/test/java/net/talaatharb/webscrap/WebScrappingApplicationTest.java
@@ -0,0 +1,15 @@
+package net.talaatharb.webscrap;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class WebScrappingApplicationTest {
+
+	@Test
+	void testProjectStarts() {
+		WebScrappingApplication.main(null);
+		assertTrue(true);
+	}
+
+}
